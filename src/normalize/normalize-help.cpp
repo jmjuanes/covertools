0b61b916d2463d04065afb320a6ab8581d885bb8
jmjuanes <josemijuanes@gmail.com>
2016-11-20 22:57:26 +0100

    src/normalize/normalize-help.cpp: added optional and mandatory arguments

--- src/normalize/normalize-help.cpp
+++ src/normalize/normalize-help.cpp
@@ -7,13 +7,20 @@ int normalize_help(void)
   cout << "Normalize the coverage matrix" << endl << endl;
 
   //Display the usage
-  cout << "Usage: covertools normalize -a <ALGORITHM> -ci <COVER_INPUT> -co <COVER_OUTPUT>" << endl << endl;
+  cout << "Usage: covertools normalize [OPTIONS] -ci <COVER_INPUT> -co <COVER_OUTPUT> -n <NUMBER_COVERAGES>" << endl << endl;
 
-  //Display the options
+  //Display the mandatory arguments
+  cout << "Mandatory: " << endl;
+  cout << "\t-ci\t"     << "Path to the input coverage file." << endl;
+  cout << "\t-co\t"     << "Path to the output coverage file." << endl;
+  cout << "\t-n\t"      << "Number of coverage columns on the coverage file" << endl;
+
+  //Add an extra line
+  cout << endl;
+
+  //Display the optional arguments
   cout << "Options: " << endl;
-  cout << "\t-a\t"    << "Select the algorithm to apply. Default is 'mean' algorithm." << endl;
-  cout << "\t-ci\t"   << "Path to the input coverage file." << endl;
-  cout << "\t-co\t"   << "Path to the output coverage file." << endl;
+  cout << "\t-a\t"      << "Select the algorithm to apply. Default is 'mean' algorithm." << endl;
 
   //Exit
   return 0;
