5ae468dd6fc22230f5c2e559db65aa0969a76a9f
jmjuanes <josemijuanes@gmail.com>
2016-11-20 22:36:31 +0100

    src/fix/fix-help.cpp: updated help. Added command information

--- src/fix/fix-help.cpp
+++ src/fix/fix-help.cpp
@@ -2,22 +2,24 @@
 int fix_help(void)
 {
   //Display the help
-  cout << "Cover Fix " << _VERSION << endl;
+  cout << "Cover Fix " << endl;
+  cout << "Version: " << _VERSION << endl;
+  cout << "Add all positions on the coverage matrix that has zero depth." << endl << endl;
 
   //Display the usage
-  cout << "Usage: covertools fix -bed <BED_FILE> -cover <COVER_FILE> -out <OUT_FILE>" << endl << endl;
+  cout << "Usage: covertools fix -bed <BED_FILE> -ci <COVER_INPUT> -co <COVER_OUTPUT>" << endl << endl;
 
   //Options
   cout << "Options:" << endl;
 
   // -bed option
-  cout << "\t-bed\t" << "Path to the BED file" << endl;
+  cout << "\t-bed\t" << "Path to the BED file with the delimited regions." << endl;
 
-  // -cover option
-  cout << "\t-cover\t" << "Path to the coverage file" << endl;
+  // -ci option
+  cout << "\t-ci\t" << "Path to the input coverage file." << endl;
 
-  // -out option
-  cout << "\t-out\t" << "Path to the output cover file" << endl;
+  // -co option
+  cout << "\t-co\t" << "Path to the output coverage file." << endl;
 
   //Exit
   return 0;
