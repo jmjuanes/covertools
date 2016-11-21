053b60ac86482059cbfa62cc343516589cc76df3
jmjuanes <josemijuanes@gmail.com>
2016-11-21 11:25:16 +0100

    src/lib/options.cpp: manage options

---
+++ src/lib/options.cpp
@@ -0,0 +1,12 @@
+//Check options
+bool checkOpt(string def_str, int def_size, string actual_str, int actual_size)
+{
+  //Check the size
+  if(def_size != actual_size){ return false; }
+
+  //Check the value
+  if(def_str != actual_str){ return false; }
+
+  //Default, return true
+  return true;
+}
