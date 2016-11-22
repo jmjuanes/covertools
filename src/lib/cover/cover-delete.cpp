72c27a3fd8b342cf4c1168899fd29af4444ac09f
jmjuanes <josemijuanes@gmail.com>
2016-11-22 10:27:01 +0100

    src/lib/cover/cover-delete.cpp: delete a coverage list

--- src/lib/cover/cover-delete.cpp
+++ src/lib/cover/cover-delete.cpp
@@ -1,41 +1,25 @@
 //Delete a cover structure
-void coverDelete(Chr & l)
+void coverDelete(CoverList & l)
 {
-  //Auxiliar pointer for chromosome
-  Chr c1 = l, c2;
+  //Auxiliar pointers
+  CoverList aux1 = l, aux2;
 
-  //Auxiliar pointers for positions
-  Pos p1, p2;
-
-  //Tab character
-  string tab = "\t";
-
-  //Read all the chromosomes
-  while(c1 != NULL)
+  //Read all the coverage values
+  while(aux1 != NULL)
   {
-    //Initialize the positions pointer
-    p1 = c1->positions;
+    //Save this coverage value
+    aux2 = aux1;
 
-    //Read all the positions
-    while(p1 != NULL)
-    {
-      //Get the actual position
-      p2 = p1;
+    //Next coverage value
+    aux1 = aux1->next;
 
-      //Save the next position
-      p1 = p1->next;
-
-      //Delete this position
-      delete p2;
-    }
-
-    //Save this chromosome
-    c2 = c1;
-
-    //Next chromosome
-    c1 = c1->next;
-
-    //Delete this chromosome
-    delete c2;
+    //Delete this coverage value
+    delete aux2;
   }
+
+  //Delete the list
+  l = NULL;
+
+  //Exit
+  return;
 }
