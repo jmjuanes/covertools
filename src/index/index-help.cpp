4589a0d69417c46406507c86c2828ed41934dbcf
jmjuanes <josemijuanes@gmail.com>
2016-11-22 14:03:05 +0100

    src/index/index-help.cpp: fixed help

--- src/index/index-help.cpp
+++ src/index/index-help.cpp
@@ -7,10 +7,11 @@ int index_help(void)
   cout << "Index a coverage file." << endl << endl;
 
   //Display the usage
-  cout << "Usage: covertools index -cover <COVER_FILE> -out <INDEX_OUTPUT_FILE>" << endl << endl;
+  cout << "Usage: covertools index -bed <BED_FILE> -cover <COVER_FILE> -out <INDEX_OUTPUT_FILE>" << endl << endl;
 
   //Display the mandatory arguments
   cout << "Mandatory: " << endl;
+  cout << "\t-bed\t"    << "Path to the BED file with the delimited regions." << endl;
   cout << "\t-cover\t"  << "Path to the input coverage file." << endl;
   cout << "\t-out\t"    << "Path to the output index file." << endl;
 
