9231467079a0ba9e9cb3ee89a469cbf8c6699606
jmjuanes <josemijuanes@gmail.com>
2016-11-22 17:00:21 +0100

    src/indel/indel-find-valid.cpp: minor update

--- src/indel/indel-find-valid.cpp
+++ src/indel/indel-find-valid.cpp
@@ -1,5 +1,5 @@
 //Calculate the differences and return if is a valid region
-bool indel_find_valid(float value1, float value2, float min, float threshold)
+bool indel_find_valid(float value1, float value2, float min_cover, float threshold)
 {
   //Calculate the first difference
   float diff1 = (value2 != 0) ? value1 / value2 : 1;
@@ -8,16 +8,16 @@ bool indel_find_valid(float value1, float value2, float min, float threshold)
   float diff2 = (value1 != 0) ? value2 / value1 : 1;
 
   //Check the first difference
-  if(min <= value1 && diff1 == 1){ diff1 = value1; }
+  if(min_cover <= value1 && diff1 == 1){ diff1 = value1; }
 
   //Check the second difference
-  if(min <= value2 && diff2 == 1){ diff2 = value2; }
+  if(min_cover <= value2 && diff2 == 1){ diff2 = value2; }
 
   //Check the first condition
   bool condition1 = (threshold <= diff1 || threshold <= diff2) ? true : false;
 
   //Get the second condition value
-  bool condition2 = (min <= value1 || min <= value2) ? true : false;
+  bool condition2 = (min_cover <= value1 || min_cover <= value2) ? true : false;
 
   //Check the conditions
   if(condition1 == false || condition2 == false)
