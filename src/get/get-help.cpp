87c516b7d236af03378f4db59f7ca3016a5c08bd
jmjuanes <josemijuanes@gmail.com>
2016-11-23 09:25:29 +0100

    src/get/get-help.cpp: updated help

--- src/get/get-help.cpp
+++ src/get/get-help.cpp
@@ -7,13 +7,14 @@ int get_help(void)
   cout << "Get coverage values of a region or list of regions." << endl << endl;
 
   //Display the usage
-  cout << "Usage: covertools get -region <REGION> -cover <COVER_FILE> -index <INDEX_FILE>" << endl << endl;
+  cout << "Usage: covertools get -region <REGION> -cover <COVER_FILE> -index <INDEX_FILE> -out <OUTPUT_FILE>" << endl << endl;
 
   //Display the mandatory arguments
   cout << "Mandatory: " << endl;
   cout << "\t-region\t" << "Wanted region name." << endl;
   cout << "\t-cover\t"  << "Path to the input coverage file." << endl;
-  cout << "\t-index\t"  << "Path to the output coverage file." << endl;
+  cout << "\t-index\t"  << "Path to the index file." << endl;
+  cout << "\t-out\t"    << "Path to the output coverage file." << endl;
 
   //Exit
   return 0;
