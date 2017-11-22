e09e7d0cc535128defa7a86634d51af368fe23f5
jmjuanes <josemijuanes@gmail.com>
2017-11-22 12:52:38 +0100

    src/get/get-help.cpp: removed out option from help

--- src/get/get-help.cpp
+++ src/get/get-help.cpp
@@ -7,14 +7,15 @@ int get_help(void)
   cout << "Get coverage values of a region or list of regions." << endl << endl;
 
   //Display the usage
-  cout << "Usage: covertools get --region <REGION> --cover <COVER_FILE> --index <INDEX_FILE> --out <OUTPUT_FILE>" << endl << endl;
+  //cout << "Usage: covertools get --region <REGION> --cover <COVER_FILE> --index <INDEX_FILE> --out <OUTPUT_FILE>" << endl << endl;
+  cout << "Usage: covertools get --region <REGION> --cover <COVER_FILE> --index <INDEX_FILE>" << endl << endl;
 
   //Display the mandatory arguments
   cout << "Mandatory options: " << endl;
   cout << " --region  " << "Wanted region name." << endl;
   cout << " --cover   " << "Path to the input coverage file." << endl;
   cout << " --index   " << "Path to the index file." << endl;
-  cout << " --out     " << "Path to the output coverage file." << endl;
+  //cout << " --out     " << "Path to the output coverage file." << endl;
 
   //Exit
   return 0;
